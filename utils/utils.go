568c0d210d78a752bd2994576a1085e15cf0571b
* <121350271+f-alotaibi@users.noreply.github.com>
2024-02-13 20:19:26 +0300

    [feat] Add size text for the paste

--- utils/utils.go
+++ utils/utils.go
@@ -17,3 +17,16 @@ func GetRawPasteContent(endpoint string) (string, error) {
 	}
 	return string(body), nil
 }
+
+func FormatStringSize(size float64) string {
+	var suffixes = []string{"B", "KB", "MB", "GB", "TB"}
+	suffixIndex := 0
+	for size > 1024 && suffixIndex < 4 {
+		suffixIndex++
+		size /= 1024
+	}
+	if suffixIndex == 0 {
+		return fmt.Sprintf("%.0f %s", size, suffixes[suffixIndex])
+	}
+	return fmt.Sprintf("%.2f %s", size, suffixes[suffixIndex])
+}
